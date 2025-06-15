c169a424c516af8d472ac650f3074ee614ee5533
Saeed-Alneyadi <122411428+Saeed-Alneyadi@users.noreply.github.com>
2025-06-15 16:52:07 +0400

    Create to_lower_case.py

---
+++ to_lower_case.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Return the lower cased version of the word
+        return s.lower()
