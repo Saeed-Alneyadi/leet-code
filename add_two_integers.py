b722d2be4e3e2cad5880724e3141a8a0ddc0c59f
Saeed-Alneyadi <122411428+Saeed-Alneyadi@users.noreply.github.com>
2025-09-16 16:28:07 +0400

    Create add_two_integers.py

---
+++ add_two_integers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        # Return the sum of the two integers
+        return num1 + num2
